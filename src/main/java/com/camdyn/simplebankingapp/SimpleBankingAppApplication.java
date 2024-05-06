1ef1747bf18bd53271af2dd869adfa83b2376a83
Camdyn Ashcraft <cashcraft16@gmail.com>
2024-05-06 13:53:16 -0600

    Reworked Gradle to match Spring project

---
+++ src/main/java/com/camdyn/simplebankingapp/SimpleBankingAppApplication.java
@@ -0,0 +1,13 @@
+package com.camdyn.simplebankingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBankingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleBankingAppApplication.class, args);
+	}
+
+}
