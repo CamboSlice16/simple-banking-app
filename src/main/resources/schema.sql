6ba928bbe98b2397262ef247c92116b5310fd409
Camdyn Ashcraft <cashcraft16@gmail.com>
2024-05-13 16:26:24 -0600

    Updating SQL data entries

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,13 +1,13 @@
 CREATE TABLE transactions (
-    transaction_id INT NOT NULL AUTO_INCREMENT,
+    transaction_id BIGINT NOT NULL AUTO_INCREMENT,
     transaction_type VARCHAR(255) NOT NULL DEFAULT '',
     amount FLOAT NOT NULL DEFAULT 0,
-    account_to VARCHAR(255),
-    account_from VARCHAR(255)
+    account_to BIGINT,
+    account_from BIGINT
 );
 
 CREATE TABLE accounts (
-    account_id INT NOT NULL AUTO_INCREMENT,
+    account_id BIGINT NOT NULL AUTO_INCREMENT,
     account_type VARCHAR(255) NOT NULL DEFAULT '',
     account_balance FLOAT NOT NULL DEFAULT 0.0
 );
